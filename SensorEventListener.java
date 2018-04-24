be941b6eee189f0854c7967c40552a5bdce1d07f
maotto1 <martinalexanderotto@gmx.net>
2018-04-24 07:26:41 +0200

    Add files via upload

---
+++ SensorEventListener.java
@@ -0,0 +1,14 @@
+package observer;
+
+import java.util.EventListener;
+
+import observable.SensorEvent;
+
+public class SensorEventListener implements EventListener {
+
+	public void onInputEvent(SensorEvent event) {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+}
